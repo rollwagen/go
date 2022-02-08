ea5d32b2f6917b8503e7e15e42bfd2d75ba6583d
rollwagen <rollwagen@gmail.com>
2022-02-08 12:15:52 +0100

    Initial commit

---
+++ learn-go-with-tests/README.md
@@ -0,0 +1,6 @@
+# Learn Go with Tests
+
+see <https://quii.gitbook.io/learn-go-with-tests/>
+
+![learn_go_icon](https://github.com/quii/learn-go-with-tests/blob/main/red-green-blue-gophers-smaller.png)
+
---
+++ learn-go-with-tests/hello-world/hello_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("Markus")
+	want := "Hello, Markus!"
+	if got != want {
+		t.Errorf("received: %q, but wanted %q", got, want)
+	}
+}
