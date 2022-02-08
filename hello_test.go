b3aef11e2725d35fa1c839fe846e6db29e4d02bd
rollwagen <rollwagen@gmail.com>
2022-02-08 19:20:44 +0100

    Implement empty input defaults to "World"

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Markus")
-	want := "Hello, Markus!"
-	if got != want {
-		t.Errorf("received: %q, but wanted %q", got, want)
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("received: %q, but wanted %q", got, want)
+		}
 	}
+
+	t.Run("say hello to people/names", func(t *testing.T) {
+		got := Hello("Markus")
+		want := "Hello, Markus!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World!' when no name is given", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
 }
